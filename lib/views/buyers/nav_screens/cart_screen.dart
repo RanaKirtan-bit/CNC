13b157ed3842f2a1638e6a98c0905ef1e4c8699e
RK <ranakirtan9@gmail.com>
2024-02-19 12:07:08 +0530

    cart and product displayed successfully

--- lib/views/buyers/nav_screens/cart_screen.dart
+++ lib/views/buyers/nav_screens/cart_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clickncart/models/product_model.dart';
 import 'package:clickncart/firebase_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 import '../../../controllers/auth_controller.dart';
 
@@ -16,11 +18,17 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   final FirebaseService _service = FirebaseService();
   List<Product> cartItems = [];
+   late Razorpay _razorpay;
+
 
   @override
   void initState() {
     super.initState();
     _loadCartItems();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
   }
 
   Future<void> _loadCartItems() async {
@@ -163,7 +171,9 @@ class _CartScreenState extends State<CartScreen> {
                   style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 ElevatedButton(
-                  onPressed: _placeOrder,
+                  onPressed: (){
+                    launchPayment();
+                  },
                   child: Text('Place Order'),
                   style: ElevatedButton.styleFrom(
                     padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
@@ -178,6 +188,64 @@ class _CartScreenState extends State<CartScreen> {
       ),
     );
   }
+
+
+  void launchPayment() async {
+    double amt =double.parse(_calculateTotalPrice());
+    var options = {
+      'key': 'rzp_test_UgjuCBp0lFcBCP', //<-- your razorpay api key/test or live mode goes here.
+      'amount':  (amt * 100).toInt(),
+      'name': 'ClickNCart',
+      'description': 'Paymnet For Your Products',
+      'prefill': {'contact': widget.userDetails.phoneNumber, 'email': widget.userDetails.email},
+      'external': {'wallets': ['Gpay']}
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint(e as String?);
+    }
+  }
+
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: 'Error ' + response.code.toString() + ' ' + response.message.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: 'Payment Success ' + response.paymentId.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.black,
+        fontSize: 16.0);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: 'Wallet Name ' + response.walletName.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.black,
+        fontSize: 16.0);
+  }
+
+
+
+
+
 
   String _calculateTotalPrice() {
     double total = 0;
